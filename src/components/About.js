fcdf1bfcc028963db19f3e16aed8b688401bce82
David Mazza <DavidMazza@Davids-MBP.attlocal.net>
2022-02-22 15:59:45 -0600

    update About section

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,9 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-3/4 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white"> Hi, I'm David <br/>
-          <br className="hidden lg:inline-block"/>I love to build amazing apps.
+          <br className="hidden lg:inline-block"/>I love to build amazing apps!
           </h1>
-          <p className="mb- leading-relaxed"> I am an innovative software engineer that is accomplishment-driven focused on sports tech. Growing up, I tried to participate in as many different sports as possible. I was fascinated with the data and performance analytics. I ended up coaching college/prep football for 7 years. Always being the guy spending late nights building long spreadsheets. During those coaching experiences I excelled in teamwork and perseverance. Looking forward, I’d love to build sports solutions through software development that improves performance analytics. Blending measurables, stats, injuries, and coaching evaluations. 
-          </p>
+
           <div className="flex justify-center">
             <a href="#contact" className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-300 rounded text-lg">
             Work With Me
@@ -25,7 +24,9 @@ export default function About() {
             src="./davidPic.jpg"
             />
           </div>
-
+          <br/>
+          <p className="mb- leading-relaxed"> I am an inspired software engineer that is accomplishment-driven, focused on sports tech. Growing up, I was passionate about sports and fascinated with the data and performance analytics. I coached college/prep football for 7 years. Priding myself on being the guy spending late nights building complex spreadsheets. During those coaching experiences I excelled in teamwork and perseverance. Looking forward, I’d love to build sports solutions through software development that improves performance analytics. Blending measurables, stats, injuries, and coaching evaluations.
+          </p>
         </div>
       </div>
     </section>
